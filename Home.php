6f785a5391fbb2c3bed103cb22ac54dc33bc2732
Meyzuun <meyzuun2@gmail.com>
2026-09-19 18:22:53 +0300

    Initial commit

---
+++ Home.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+
+
+    <?php // PHP OPEN TAGE
+
+    //DISPLAY MESSGE USING FOE ECHO OR PRINT
+
+     echo"<h1> Welcome  to Home PHP </h1>";
+
+     print"<h2>Meymuun<h2/>";
+
+     echo("<h2>Helo</h2>");
+
+
+      // ECHO DISPLAY ARGUMENT WITHOUT PRESENTHISIS
+     echo "Meymuun", "Abuukar";
+     echo "<br>";
+
+     echo ("QA");
+     echo "<br>";
+    $Fullname = "Meyzuun Abuukar Ali ";
+    echo"My name is: $Fullname";
+    echo "<br>";
+
+    $Lastname= "Ali";
+    echo"my last name is :$Lastname";
+    echo "<br>";
+
+
+
+    // Worlds  uu xisaabim
+    $my_str= "Welcome to PHP Republic";
+    echo str_word_count($my_str);
+    echo "<br>";
+
+
+    //Asi dhan uu xisabin
+
+    $my_str= "Welcome to CA233 CLASS";
+     echo strlen($my_str);
+     echo "<br>";
+
+
+    // PHP END TAG
+
+    ?>
+    
+</body>
+</html>
